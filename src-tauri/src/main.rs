ddabb698d790eb9156e4502f9def20bdacf004c4
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-05-13 13:13:59 -0400

    backend changes to try and think of good way to code simulation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod simulation;
+pub mod players;
+pub mod leagues;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
---
+++ src-tauri/src/simulation/sim.rs
@@ -0,0 +1,16 @@
+
+pub fn simulate_game(){
+
+}
+
+pub fn simulate_season(){
+    
+}
+
+pub fn simulate_academy(){
+
+}
+
+pub fn schedule_season(){
+    
+}
